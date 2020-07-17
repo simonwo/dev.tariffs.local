00f825b74a4fa5add751085d601f7a6ec1a88f8f
Simon Worthington <simon.worthington@digital.trade.gov.uk>
2020-07-17 10:25:48 +0100

    Fix some bugs in the commodity code script.

--- snapshot/code_parents.sql
+++ snapshot/code_parents.sql
@@ -40,7 +40,39 @@ CREATE INDEX ON ccs (next_sid);
 -- which is to control how many indents are inserted in a document to give
 -- a visual tree structure, and chapter headings are not indented.
 -- So we fix this by setting chapters (HS2) to -1 to create the right structure.
-WITH parents AS (
+WITH
+live_codes AS (
+  SELECT
+    *
+  FROM
+    goods_nomenclatures
+  WHERE
+    (
+      goods_nomenclatures.validity_end_date IS NULL
+      OR goods_nomenclatures.validity_end_date >= NOW()
+    )
+),
+goods_indents AS (
+  SELECT
+    *
+  FROM
+    (
+      SELECT
+        g.goods_nomenclature_sid,
+        gi.number_indents,
+        ROW_NUMBER() OVER (
+          PARTITION BY g.goods_nomenclature_sid
+          ORDER BY
+            gi.validity_start_date DESC
+        ) AS row
+      FROM
+        live_codes AS g
+        LEFT OUTER JOIN goods_nomenclature_indents AS gi ON g.goods_nomenclature_sid = gi.goods_nomenclature_sid
+    ) AS latest_desc
+  WHERE
+    latest_desc.row = 1
+),
+parents AS (
   SELECT
     parents.*,
     (
@@ -50,8 +82,8 @@ WITH parents AS (
       END
     ) AS number_indents
   FROM
-    goods_nomenclatures AS parents
-    LEFT OUTER JOIN goods_nomenclature_indents AS parent_indents ON parents.goods_nomenclature_sid = parent_indents.goods_nomenclature_sid
+    live_codes AS parents
+    LEFT OUTER JOIN goods_indents AS parent_indents ON parents.goods_nomenclature_sid = parent_indents.goods_nomenclature_sid
 )
 INSERT INTO
   ccs
@@ -59,15 +91,23 @@ SELECT
   parents.goods_nomenclature_item_id as cc,
   parents.goods_nomenclature_sid AS sid,
   parents.number_indents as indent,
-  LEAD(parents.goods_nomenclature_item_id) OVER (
-    PARTITION BY parents.number_indents
-    ORDER BY
-      parents.goods_nomenclature_item_id
+  -- The last code at each indent level will have NULL
+  -- in its next column. For these codes we must extend
+  -- the window to infinity to capture any kids that come after.
+  COALESCE(
+    LEAD(parents.goods_nomenclature_item_id) OVER (
+      PARTITION BY parents.number_indents
+      ORDER BY
+        parents.goods_nomenclature_item_id,
+        parents.producline_suffix
+    ),
+    '9999999999'
   ) AS next,
   LEAD(parents.producline_suffix) OVER (
     PARTITION BY parents.number_indents
     ORDER BY
-      parents.goods_nomenclature_item_id
+      parents.goods_nomenclature_item_id,
+      parents.producline_suffix
   ) AS next_suffix,
   LEAD(parents.goods_nomenclature_sid) OVER (
     PARTITION BY parents.number_indents
@@ -113,15 +153,26 @@ FROM
   ccs as cc1,
   ccs as cc2
 WHERE
-  cc2.cc > cc1.cc
+  cc2.cc >= cc1.cc
   AND cc2.cc < cc1.next
-  AND cc2.producline_suffix <= cc1.next_suffix;
+  AND cc2.indent > cc1.indent;
 
 -- The start of the actual query. The first two tables link each code
 -- to the most recent description and indent, both of which can change
 -- independently. We also take the opportunity to find the number of
 -- trailing zeroes in the commodity code, which tells us the HS level.
-WITH goods_descriptions AS (
+WITH live_codes AS (
+  SELECT
+    *
+  FROM
+    goods_nomenclatures
+  WHERE
+    (
+      goods_nomenclatures.validity_end_date IS NULL
+      OR goods_nomenclatures.validity_end_date >= NOW()
+    )
+),
+goods_descriptions AS (
   SELECT
     *,
     CHAR_LENGTH(
@@ -139,7 +190,7 @@ WITH goods_descriptions AS (
             gdp.validity_start_date DESC
         ) AS row
       FROM
-        goods_nomenclatures_oplog AS g
+        live_codes AS g
         LEFT OUTER JOIN goods_nomenclature_description_periods AS gdp ON g.goods_nomenclature_sid = gdp.goods_nomenclature_sid
         LEFT OUTER JOIN goods_nomenclature_descriptions AS gd ON gdp.goods_nomenclature_description_period_sid = gd.goods_nomenclature_description_period_sid
     ) AS latest_desc
@@ -160,42 +211,52 @@ goods_indents AS (
             gi.validity_start_date DESC
         ) AS row
       FROM
-        goods_nomenclatures AS g
+        live_codes AS g
         LEFT OUTER JOIN goods_nomenclature_indents AS gi ON g.goods_nomenclature_sid = gi.goods_nomenclature_sid
     ) AS latest_desc
   WHERE
     latest_desc.row = 1
+),
+codes AS (
+  SELECT
+    goods_nomenclatures.goods_nomenclature_sid :: integer AS "SID",
+    goods_nomenclatures.goods_nomenclature_item_id AS "Commodity code",
+    goods_nomenclatures.producline_suffix :: integer AS "Suffix",
+    -- Round up the trailing zeros to the nearest multiple of 2 – that's the HS level.
+    goods_indents.number_indents :: integer AS "Indent",
+    ( goods_descriptions.trailing_zeroes + MOD(goods_descriptions.trailing_zeroes, 2) ) :: integer AS "HS Level",
+    goods_descriptions.description AS "Description",
+    goods_parents.parent_sid :: integer AS "Parent SID",
+    goods_parents.parent_cc AS "Parent code",
+    goods_parents.parent_suffix :: integer AS "Parent suffix",
+    -- Fix the -1 in chapter headings back to their "correct" zero.
+    GREATEST(0, goods_parents.parent_indent) :: integer AS "Parent indent"
+  FROM
+    live_codes AS goods_nomenclatures
+    LEFT OUTER JOIN goods_indents ON goods_nomenclatures.goods_nomenclature_sid = goods_indents.goods_nomenclature_sid
+    LEFT OUTER JOIN goods_descriptions ON goods_descriptions.goods_nomenclature_sid = goods_nomenclatures.goods_nomenclature_sid
+    LEFT OUTER JOIN cc_children AS goods_parents ON goods_nomenclatures.goods_nomenclature_sid = goods_parents.child_sid
+  WHERE
+    (
+      goods_parents.parent_indent = goods_indents.number_indents - 1
+      OR goods_parents.parent_sid IS NULL
+    )
+  ORDER BY
+    goods_nomenclatures.goods_nomenclature_item_id,
+    goods_indents.number_indents
 )
+-- Some sanity checks:
+-- Show any parents that are not in the list, should be empty:
+--    SELECT * FROM goods_nomenclatures
+--    LEFT OUTER JOIN codes on codes."Parent SID" = goods_nomenclatures.goods_nomenclature_sid
+--    WHERE "Parent SID" not in (SELECT "SID" FROM codes)
+-- Show any codes from the main list that are not present, should be empty:
+--    SELECT * FROM live_codes AS goods_nomenclatures
+--    WHERE goods_nomenclature_sid NOT IN (SELECT "SID" from codes)
+-- Show how many parents each code has, should be at most 1:
+--    SELECT "SID", COUNT("Parent SID") FROM codes
+--    GROUP BY "SID" ORDER BY COUNT DESC
 SELECT
-  goods_nomenclatures.goods_nomenclature_sid :: integer AS "SID",
-  goods_nomenclatures.goods_nomenclature_item_id AS "Commodity code",
-  goods_nomenclatures.producline_suffix :: integer AS "Suffix",
-  goods_indents.number_indents :: integer AS "Indent",
-  -- Round up the trailing zeros to the nearest multiple of 2 – that's the HS level.
-  ( goods_descriptions.trailing_zeroes + MOD(goods_descriptions.trailing_zeroes, 2) ) :: integer AS "HS Level",
-  goods_descriptions.description AS "Description",
-  goods_parents.parent_sid :: integer AS "Parent SID",
-  goods_parents.parent_cc AS "Parent code",
-  goods_parents.parent_suffix :: integer AS "Parent suffix",
-  -- Fix the -1 in chapter headings back to their "correct" zero.
-  GREATEST(0, goods_parents.parent_indent) :: integer AS "Parent indent"
+  *
 FROM
-  goods_nomenclatures
-  LEFT OUTER JOIN goods_indents ON goods_nomenclatures.goods_nomenclature_sid = goods_indents.goods_nomenclature_sid
-  LEFT OUTER JOIN goods_descriptions ON goods_descriptions.goods_nomenclature_sid = goods_nomenclatures.goods_nomenclature_sid
-  LEFT OUTER JOIN cc_children AS goods_parents ON goods_nomenclatures.goods_nomenclature_sid = goods_parents.child_sid
-WHERE
-  -- Only select comm codes which are "current".
-  (
-    goods_nomenclatures.validity_end_date IS NULL
-    or goods_nomenclatures.validity_end_date >= NOW()
-  )
-  -- Only select parents that actually a parent (the ccs table
-  -- also contains higher level ancestors, e.g. grandparents)
-  AND (
-    goods_parents.parent_indent = goods_indents.number_indents - 1
-    OR goods_parents.parent_sid IS NULL
-  )
-ORDER BY
-  goods_nomenclatures.goods_nomenclature_item_id,
-  goods_indents.number_indents
+  codes
